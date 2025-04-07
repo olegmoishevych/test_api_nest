a9a3fe2b7b5f86d1ce0353a51f3fe7413596f4d7
olehmoishevych <olegmoishevych@gmail.com>
2025-04-07 23:22:14 +0300

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { LogsModule } from './modules/logs/logs.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LogEntity } from './modules/logs/domain/logs';
+import * as process from 'node:process';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [LogsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [LogEntity],
+      synchronize: true,
+    }),
+    LogsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const PORT = process.env.PORT ?? 3000;
 
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
